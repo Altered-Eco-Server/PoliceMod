4d9ce969994718450f43a58ccf7e989438cf8e4b
AlteredMinds <116692103+AlteredMinds@users.noreply.github.com>
2023-01-26 07:38:51 -0600

    added json for police config and sqashed bugs

--- PoliceMod/PoliceConfig.cs
+++ PoliceMod/PoliceConfig.cs
@@ -11,19 +11,19 @@ namespace Eco.Plugins
     [Serialized]
     public class PoliceConfig
     {
-        [Serialized] public static string logDir { get; set; } = @"C:\Users\luluq\Desktop\AlteredEco";
+        [Serialized] public static string logDir { get; set; } = @"C:\Users\c_bat\Desktop\AlteredEco";
 
         [Serialized] public static int Cell1PosX { get; set; } = 561;
         [Serialized] public static int Cell1PosY { get; set; } = 55;
         [Serialized] public static int Cell1PosZ { get; set; } = 518;
 
-        [Serialized] public static int Cell2PosX { get; set; } = 546;
+        [Serialized] public static int Cell2PosX { get; set; } = 561;
         [Serialized] public static int Cell2PosY { get; set; } = 55;
-        [Serialized] public static int Cell2PosZ { get; set; } = 555;
+        [Serialized] public static int Cell2PosZ { get; set; } = 514;
 
-        [Serialized] public static int ImpoundPosX { get; set; } = 420;
-        [Serialized] public static int ImpoundPosY { get; set; } = 55;
-        [Serialized] public static int ImpoundPosZ { get; set; } = 100;
+        [Serialized] public static int ImpoundPosX { get; set; } = 470;
+        [Serialized] public static int ImpoundPosY { get; set; } = 76;
+        [Serialized] public static int ImpoundPosZ { get; set; } = 500;
 
         [Serialized] public static Vector3i Cell1Pos = new Vector3i(Cell1PosX, Cell1PosY, Cell1PosZ);
         [Serialized] public static Vector3i Cell2Pos = new Vector3i(Cell2PosX, Cell2PosY, Cell2PosZ);
